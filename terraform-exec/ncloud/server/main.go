5da7677916fe527cb6c91568886e9a577e16e72c
devnk915 <devnk915@gmail.com>
2022-06-28 11:16:56 +0900

    Add: tf-exec destroy, apply

--- terraform-exec/ncloud/server/main.go
+++ terraform-exec/ncloud/server/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/hc-install/product"
@@ -20,6 +21,7 @@ func main() {
 	execPath, err := installer.Install(context.Background())
 	if err != nil {
 		log.Fatalf("error installing Terraform: %s", err)
+		return
 	}
 
 	//workingDir := "C:\\Users\\kng\\Documents\\GitHub\\nk915\\terraform_example\\terrform\\ncloud\\server"
@@ -27,17 +29,45 @@ func main() {
 	tf, err := tfexec.NewTerraform(workingDir, execPath)
 	if err != nil {
 		log.Fatalf("error running NewTerraform: %s", err)
+		return
 	}
+
+	tf.SetStdout(os.Stdout)
+	tf.SetStderr(os.Stderr)
 
 	err = tf.Init(context.Background(), tfexec.Upgrade(true))
 	if err != nil {
 		log.Fatalf("error running Init: %s", err)
+		return
 	}
+	result, err := tf.Plan(context.Background())
+	if err != nil {
+		log.Fatalf("error running Plan: %s", err)
+		return
+	}
+	if result == false {
+		fmt.Println("tf plan: ", result)
+		return
+	}
+
+	//	if err := tf.Destroy(context.Background()); err != nil {
+	//		fmt.Println("tf destroy success")
+	//		return
+	//	}
+	//	fmt.Println("tf destroy success")
+	//	return
+
+	if err := tf.Apply(context.Background()); err != nil {
+		log.Fatalf("error running Apply: %s", err)
+		return
+	}
+	fmt.Println("tf apply success")
 
 	state, err := tf.Show(context.Background())
 	if err != nil {
 		log.Fatalf("error running Show: %s", err)
+		return
 	}
 
-	fmt.Println(state.FormatVersion) // "0.1"
+	fmt.Println(state) // "0.1"
 }
